7e420bfe03ea3cbec664eed0fcbad3d7fb8811a1
ayushacharya27 <ayush.acharya2027@gmail.com>
2025-09-10 03:51:21 +0530

    Swarm Node added, RGB Capture Initiated, Worked finally started

---
+++ swarm_node/swarm_node/rgb_node.py
@@ -0,0 +1,29 @@
+import rclpy
+from rclpy.node import Node
+from sensor_msgs.msg import Image
+from cv_bridge import CvBridge
+import cv2
+
+class CameraPublisherNode(Node):
+    def __init__(self):
+        super().__init__('camera_publisher')
+        self.publisher = self.create_publisher(Image, 'camera/image_raw', 10)
+        self.timer = self.create_timer(0.1, self.timer_callback)  # 10 Hz
+        self.capture = cv2.VideoCapture(0) # Will add ID after selecting camera
+        self.bridge = CvBridge()
+
+    def timer_callback(self):
+        ret, frame = self.capture.read()
+        if ret:
+            msg = self.bridge.cv2_to_imgmsg(frame, encoding="bgr8")
+            self.publisher.publish(msg)
+             
+def main(args=None):
+    rclpy.init(args=args)
+    node = CameraPublisherNode()
+    rclpy.spin(node)
+    node.destroy_node()
+    rclpy.shutdown()
+
+if __name__ == '__main__':
+    main()
